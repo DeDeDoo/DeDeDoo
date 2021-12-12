8fc5c250fb9e9e244a705491acd9b3bac8f142e1
DeDeDoo <95982427+DeDeDoo@users.noreply.github.com>
2021-12-12 12:24:48 -0500

    Update wep.h

--- wep.h
+++ wep.h
@@ -3,11 +3,14 @@
 #include <vector>
 
 class weapon{
+ private:
  int DMG;
  string DMG_type; // DEX or STR or INT 
  int WGT;
  vector<string> modifiers; //Horse, Armor, Mage
   
+ public:
+   vector<weapon> genWepList(); //creates the basic weapon types used in the game
 }
 
 #endif
